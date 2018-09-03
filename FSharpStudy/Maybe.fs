124127ff139b297e756623753cf86e9015ff283f
Yoshitomo Minami <minami530@gmail.com>
2018-09-03 23:51:38 +0900

    Moved add3 function

--- FSharpStudy/Maybe.fs
+++ FSharpStudy/Maybe.fs
@@ -8,11 +8,3 @@ type MaybeBuilder() =
   member __.Return(x) = Some x
 
 let maybe = MaybeBuilder()
-
-let add3 opt1 opt2 opt3 =
-  maybe {
-    let! x = opt1
-    let! y = opt2
-    let! z = opt3
-    return x + y + z
-  }
--- FSharpStudy/Program.fs
+++ FSharpStudy/Program.fs
@@ -3,6 +3,14 @@
 
 open Maybe
 
+let add3 opt1 opt2 opt3 =
+  maybe {
+    let! x = opt1
+    let! y = opt2
+    let! z = opt3
+    return x + y + z
+  }
+
 [<EntryPoint>]
 let main _ = 
     let a= Some 3
