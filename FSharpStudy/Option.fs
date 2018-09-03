cebd19b924d5f2c49e322090f4580cc47a38f068
Yoshitomo Minami <minami530@gmail.com>
2018-09-03 23:06:18 +0900

    Added F# project

---
+++ FSharpStudy/Option.fs
@@ -0,0 +1,18 @@
+﻿module Option
+
+type OptionBuilder() =
+  member __.Bind(x, f) =
+    match x with
+    | Some v -> f v
+    | None -> None
+  member __.Return(x) = Some x
+
+let option = OptionBuilder()
+
+let tryOption opt1 opt2 opt3 =
+  option {
+    let! x = opt1
+    let! y = opt2
+    let! z = opt3
+    return x + y + z
+  }
---
+++ FSharpStudy/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Option
+
+[<EntryPoint>]
+let main _ = 
+    let x = Some 3
+    let y = Some 2
+    let z = Some 4
+    let opt = tryOption x y z
+    match opt with
+    | Some v -> printfn "Some: %A" v
+    | None -> printfn "None"
+    0
